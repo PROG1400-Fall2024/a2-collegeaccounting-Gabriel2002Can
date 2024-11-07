b163cb6d14bc3e8e3103baba2dea220f6fbd2b57
Gabriel2002Can <gabriel2002ca@gmail.com>
2024-11-07 17:22:02 -0400

    Creating initial variables and methods

--- Staff.java
+++ Staff.java
@@ -1,3 +1,16 @@
-public class Staff {
+public class Staff extends Person{
+    private int year;
 
+    public Staff(String name, String address, int year) {
+        super(name, address);
+        this.year = year;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,20 @@
-public class Student {
+public class Student extends Person{
+    private int yearsOfService;
 
+    public Student(String name, String address, int yearsOfService){
+        super(name,address);
+        this.yearsOfService = yearsOfService;
+    }
+
+    public int getYearsOfService(){
+        return yearsOfService;
+    }
+
+    public void setYearsOfService(int yearsOfService){
+        this.yearsOfService = yearsOfService;
+    }
+
+    public static Student createStudent(String name, String address, int yearsOfService){
+        return new Student(name,address,yearsOfService);
+    }
 }
